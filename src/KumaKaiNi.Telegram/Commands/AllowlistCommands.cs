718141c7439e48777d45cd53b8636f038a6126e6
rekyuu <senkan.re.kyuu@gmail.com>
2024-05-01 05:37:23 -0600

    Re-added Telegram allow command

---
+++ src/KumaKaiNi.Telegram/Commands/AllowlistCommands.cs
@@ -0,0 +1,36 @@
+using KumaKaiNi.Core.Attributes;
+using KumaKaiNi.Core.Database;
+using KumaKaiNi.Core.Database.Entities;
+using KumaKaiNi.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KumaKaiNi.Telegram.Commands;
+
+public static class AllowlistCommands
+{
+    [Command(["approve", "allow"], UserAuthority.Administrator)]
+    public static async Task<KumaResponse?> AddToAllowListAsync(KumaRequest request)
+    {
+        if (request.ChannelId == null) return null;
+        
+        await using KumaKaiNiDbContext db = new();
+
+        TelegramAllowList? allowList = await db.TelegramAllowList
+            .FirstOrDefaultAsync(x => x.ChannelId == request.ChannelId);
+
+        if (allowList?.Approved == true) return new KumaResponse("Chat is already approved.");
+
+        if (allowList == null)
+        {
+            allowList = new TelegramAllowList { ChannelId = request.ChannelId.Value };
+            await db.TelegramAllowList.AddAsync(allowList);
+        }
+
+        allowList.Approved = true;
+        allowList.Warnings = 0;
+
+        await db.SaveChangesAsync();
+
+        return new KumaResponse("Added to allowlist.");
+    }
+}
