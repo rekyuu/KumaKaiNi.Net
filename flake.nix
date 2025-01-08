292b6b325bc7b8257694ae4b210317226356374f
rekyuu <senkan.re.kyuu@gmail.com>
2025-01-08 11:16:09 -0700

    Add nix stuff

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    };
+
+    outputs = { self, nixpkgs }:
+        let
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            dotnet = pkgs.dotnetCorePackages.sdk_8_0;
+        in {
+            devShells.x86_64-linux.default = pkgs.mkShell {
+                nativeBuildInputs = with pkgs; [
+                    dotnet
+                    dotnet-ef
+                ];
+            };
+        };
+}
