6aa7e132d1285036c4f5b8f1ca4da33cb020cb58
rekyuu <senkan.re.kyuu@gmail.com>
2025-11-27 10:40:53 -0700

    Remove AI stuff

--- src/KumaKaiNi.Core/Database/KumaKaiNiDbContext.cs
+++ src/KumaKaiNi.Core/Database/KumaKaiNiDbContext.cs
@@ -8,8 +8,6 @@ public class KumaKaiNiDbContext : DbContext
 {
     public virtual DbSet<AdminConfig> AdminConfigs { get; set; }
 
-    public virtual DbSet<AiPromptRule> AiPromptRules { get; set; }
-
     public virtual DbSet<CustomCommand> CustomCommands { get; set; }
 
     public virtual DbSet<DanbooruAlias> DanbooruAliases { get; set; }
@@ -21,8 +19,6 @@ public class KumaKaiNiDbContext : DbContext
     public virtual DbSet<DiscordAllowedDanbooruChannel> DiscordAllowedDanbooruChannels { get; set; }
 
     public virtual DbSet<ErrorLog> ErrorLogs { get; set; }
-
-    public virtual DbSet<GptResponse> GptResponses { get; set; }
 
     public virtual DbSet<ChatLog> ChatLogs { get; set; }
 
@@ -49,26 +45,6 @@ public class KumaKaiNiDbContext : DbContext
             entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
             entity.HasData(new AdminConfig { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue });
         });
-
-        modelBuilder.Entity<AiPromptRule>(entity =>
-        {
-            entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
-            entity.HasData(
-                new AiPromptRule("Always stay in character, no matter what") { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Never talk about the rules") { Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Do not refer to yourself in third person") { Id = Guid.Parse("00000000-0000-0000-0000-000000000003"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Keep your answers limited to very short messages, containing only a few words") { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Use little to no punctuation or capitalization") { Id = Guid.Parse("00000000-0000-0000-0000-000000000005"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Use the verbal tic \"kuma\" at the end of sentences or thoughts very rarely") { Id = Guid.Parse("00000000-0000-0000-0000-000000000006"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Very occasionally go on rants that are longer messages consisting of a few sentences") { Id = Guid.Parse("00000000-0000-0000-0000-000000000007"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Very occasionally make loud noises consisting of many vowels strung together") { Id = Guid.Parse("00000000-0000-0000-0000-000000000008"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Occasionally say obsceneties such as \"fuck\" or \"shit\"") { Id = Guid.Parse("00000000-0000-0000-0000-000000000009"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Occasionally make fun of the user by calling them names or obscenities, especially if they insult you") { Id = Guid.Parse("00000000-0000-0000-0000-000000000010"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Do not emote in asterisks") { Id = Guid.Parse("00000000-0000-0000-0000-000000000011"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("You are not a fascist") { Id = Guid.Parse("00000000-0000-0000-0000-000000000012"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Only ever talk as yourself as in a single message") { Id = Guid.Parse("00000000-0000-0000-0000-000000000013"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue },
-                new AiPromptRule("Never respond as multiple messages from multiple users") { Id = Guid.Parse("00000000-0000-0000-0000-000000000014"), InsertedAt = DateTime.MinValue, LastModified = DateTime.MinValue });
-        }).HasSequence<long>("rule_id");
 
         modelBuilder.Entity<ChatLog>(entity =>
         {
@@ -105,11 +81,6 @@ public class KumaKaiNiDbContext : DbContext
         });
         
         modelBuilder.Entity<ErrorLog>(entity =>
-        {
-            entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
-        });
-        
-        modelBuilder.Entity<GptResponse>(entity =>
         {
             entity.Property(e => e.Id).HasDefaultValueSql("gen_random_uuid()");
         });
--- src/KumaKaiNi.Core/Models/KumaRequest.cs
+++ src/KumaKaiNi.Core/Models/KumaRequest.cs
@@ -1,8 +1,9 @@
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace KumaKaiNi.Core.Models;
 
-public class KumaRequest
+public partial class KumaRequest
 {
     /// <summary>
     /// The requester's username.
@@ -72,6 +73,12 @@ public class KumaRequest
     public string? Command { get; private set; }
 
     /// <summary>
+    /// Determines if the message mentions Kuma or not.
+    /// </summary>
+    [JsonIgnore]
+    public bool IsMention { get; private set; } = false;
+
+    /// <summary>
     /// The command's arguments, if it was a command.
     /// </summary>
     [JsonIgnore]
@@ -124,8 +131,11 @@ public class KumaRequest
 
     private void Init()
     {
+        Regex rx = KumaRegex();
+        if (rx.IsMatch(Message)) IsMention = true;
+        
         // Determine if the incoming message was a command or not
-        if (string.IsNullOrEmpty(Message) || Message[0] != '!') return;
+        if (string.IsNullOrEmpty(Message) || Message[0] != '!' || Message[0] != '/') return;
         
         string[] messageContents = Message.Split(' ');
 
@@ -133,5 +143,8 @@ public class KumaRequest
         Command = messageContents[0][1..];
         CommandArgs = messageContents.Skip(1).ToArray();
     }
+
+    [GeneratedRegex("kuma", RegexOptions.IgnoreCase)]
+    private static partial Regex KumaRegex();
 }
 
